c3b5b6d35d28c22510c5a62f6fd7e8d5bb311709
Ismail-Taha <taha.ismaile2002@gmail.com>
2023-11-02 14:56:50 -0700

    adv task

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-
-if __name__ == "__main":
+if __name__ == "__main__":
     from sys import argv
     from calculator_1 import add, sub, mul, div
     if len(argv) != 4:
         print("Usage:", argv[0], "<a> <operator> <b>")
         exit(1)
 
-    operator = argv[2]
-    list_opr = {'+': add, '-': sub, '*': mul, '/': div}
-    if operator not in list_opr:
+    opr = argv[2]
+    ls_opr = {'+': add, '-': sub, '*': mul, '/': div}
+    if opr not in ls_opr:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
 
     a = int(argv[1])
     b = int(argv[3])
-    print('{:d} {:s} {:d} = {:d}'.format(a, operator, a, list_opr[operator](a, b)))
+    print('{:d} {:s} {:d} = {:d}'.format(a, opr, b, ls_opr[opr](a, b)))
