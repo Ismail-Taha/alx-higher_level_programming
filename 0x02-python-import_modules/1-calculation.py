8d351856218cc62d1981e29488971f21203af06d
Ismail-Taha <taha.ismaile2002@gmail.com>
2023-11-02 13:55:31 -0700

    fix task1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,21 +1,12 @@
 #!/usr/bin/python3
 
-# Import functions from calculator_1 module
 from calculator_1 import add, sub, mul, div
 
-# Define variables a and b
 a = 10
 b = 5
 
-# Perform mathematical operations using the imported functions
-addition = add(a, b)
-substraction = sub(a, b)
-multiplication = mul(a, b)
-devision = div(a, b)
-
-# Print the results with a single print statement for each
-print(f"{a} + {b} = {addition}")
-print(f"{a} - {b} = {substraction}")
-print(f"{a} * {b} = {multiplication}")
-print(f"{a} / {b} = {devision}")
-
+if __name__ == '__main__':
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
+    print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
