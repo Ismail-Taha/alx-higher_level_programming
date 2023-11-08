7d13cfd2cfbb9b4186586904365a8c8aef37cf2e
Ismail-Taha <taha.ismaile2002@gmail.com>
2023-11-08 12:57:55 -0800

    task12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    rvalues = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50,
+        'C': 100, 'D': 500, 'M': 1000
+    }
+
+    res = 0
+    prevalue = 0
+    len_roman = len(roman_string)
+
+    for i in range(len_roman - 1, -1, -1):
+        curvalue = rvalues.get(roman_string[i], 0)
+
+        if curvalue < prevalue:
+            res -= curvalue
+        else:
+            res += curvalue
+
+        prevalue = curvalue
+
+    return res
