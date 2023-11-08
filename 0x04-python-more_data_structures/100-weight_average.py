9f5a0f86b376618aa6bc2c0b16763a3bd960e257
Ismail-Taha <taha.ismaile2002@gmail.com>
2023-11-08 14:27:02 -0800

    adv task

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,12 @@
+def weight_average(my_list=[]):
+    if my_list is not None:
+        total_weighted_sum = 0
+        total_occurrences = 0
+        
+        for weight, occurrence in my_list:
+            total_weighted_sum += weight * occurrence
+            total_occurrences += occurrence
+        
+        return total_weighted_sum / total_occurrences if total_occurrences > 0 else 0
+    else:
+        return 0
