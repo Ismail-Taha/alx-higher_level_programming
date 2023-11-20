3f9cdceeeb6e35b45fe807a79d0a7f856f2f891f
Ismail-Taha <taha.ismaile2002@gmail.com>
2023-11-20 12:58:50 -0800

    fix adv task

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,5 +6,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return (True)
     except (TypeError, ValueError) as err:
-        print("Exception:", err, file=stderr)
+        print("Exception: {}".format(err), file=stderr)
         return (False)
