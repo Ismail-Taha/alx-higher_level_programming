48d6563aa48eeb45b8c49903e4e0584a78d1617b
Ismail-Taha <taha.ismaile2002@gmail.com>
2023-11-27 10:33:31 -0800

    task2

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""Defines a Rectangle class."""
+
+"""
+Module 2-rectangle
+Contains class Rectangle with private attribute width and height,
+and public area and perimeter methods
+"""
+
+
+class Rectangle:
+    """
+    Defines class rectangle with private attribute width & height.
+    Args:
+        width (int): width
+        height (int): height
+    """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ Setter sets width for int > 0 """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (2 * self.__width) + (2 * self.height)
