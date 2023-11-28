ebc963bffd5601c6957b53f496659bb91cfc8e7c
Ismail-Taha <taha.ismaile2002@gmail.com>
2023-11-27 16:53:43 -0800

    task7

---
+++ 0x08-python-more_classes/7-rectangle.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+"""Defines a Rectangle class."""
+
+
+class Rectangle():
+    """
+        Defines class rectangle with private attribute width and height.
+            Args:
+                width (int): width
+                height (int): height
+            Attributes:
+                number_of_instances (int): number of instc created and not deleted
+                print_symbol (any type): used to print str representation
+    """
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        """ Initialize rectangles """
+        self.width = width
+        self.height = height
+        type(self).number_of_instances += 1
+
+    def __del__(self):
+        """ Deletes instance of class """
+        print("Bye rectangle...")
+        type(self).number_of_instances -= 1
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """ Return width * height """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """ Return 2*width + 2*height (or return 0 if width or height is 0) """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (2 * self.__width) + (2 * self.height)
+
+    def __str__(self):
+        """ Prints rectangle with #'s """
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        rectg = "\n".join([str(self.print_symbol) * self.__width
+                         for rows in range(self.__height)])
+        return rectg
+
+    def __repr__(self):
+        """ String to recreate new instance """
+        return "Rectangle({:d}, {:d})".format(self.width, self.height)
