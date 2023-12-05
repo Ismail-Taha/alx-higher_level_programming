380645f0c9a05fdc5a1eb0377cb18ee52e3e24ea
Ismail-Taha <taha.ismaile2002@gmail.com>
2023-12-05 13:47:28 -0800

    fix task6

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -2,6 +2,7 @@
 """Defines a function to read a Python object from a JSON file."""
 import json
 
+
 def load_from_json_file(filename):
     """Creates a Python object from a JSON file."""
     with open(filename) as o:
