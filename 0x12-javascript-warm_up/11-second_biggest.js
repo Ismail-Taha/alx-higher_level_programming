7545e9431a26fd9e6c7fec25d29fa2276b5b6343
Ismail-Taha <taha.ismaile2002@gmail.com>
2024-02-13 23:17:47 +0100

    task11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,6 +4,6 @@ if (process.argv.length <= 3) {
   } else {
     const args = process.argv.map(Number)
       .slice(2, process.argv.length)
-      .sort((a, b) => a - b);
+      .sort((n1, n2) => n1 - n2);
     console.log(args[args.length - 2]);
   }
