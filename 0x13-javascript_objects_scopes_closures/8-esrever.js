ace903c92eccbad502d4437383d74ef274b5d4b9
Ismail-Taha <taha.ismaile2002@gmail.com>
2024-02-14 18:47:13 -0800

    styl

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
 
-module.exports.esrever = function (list) {
-    const reversedList = [];
-    for (let i = list.length - 1; i >= 0; i--) {
-      reversedList[list.length - i - 1] = list[i];
-    }
-    return reversedList;
-  };
+exports.esrever = function (list) {
+  const rev = [];
+  let k = 0;
+  for (let i = list.length - 1; i >= 0; i--) {
+    rev[j] = list[i];
+    k++;
+  }
+  return (rev);
+};
