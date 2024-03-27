169bd58efdbec707609d228ec120d3adc90c86aa
Ismail-Taha <taha.ismaile2002@gmail.com>
2024-03-26 19:30:28 -0700

    fix

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 const req = require('request');
 req(process.argv[2], function (err, res, body) {
-  if (!err) {
-    const films = JSON.parse(body).results;
-    console.log(films.reduce((acc, film) => {
-      return film.characters.find((char) => char.endsWith('/18/'))
-	? acc + 1
-	: acc;
+if (!err) {
+	const films = JSON.parse(body).results;
+	console.log(films.reduce((acc, film) => {
+	return film.characters.find((char) => char.endsWith('/18/'))
+		? acc + 1: acc;
     }, 0));
   }
 });
